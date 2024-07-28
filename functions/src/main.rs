be5abdb7cc7d6099e6458766a26798aeb5851be0
HenrikHorpedal <hhorpedal@gmail.com>
2024-07-28 15:21:53 +0200

    feat: add dtypes and functions.

---
+++ functions/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    another_function();
+    yet_another_function(6);
+    print_labeled_measurement(5, 'h');
+}
+
+fn another_function() {
+    println!("Hello from another functions.")
+}
+
+fn yet_another_function(x: i32) {
+    println!("The value of x is {x}.")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
