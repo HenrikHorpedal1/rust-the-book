6ffe63ac2b394632725372bb6c93531eed9dde0e
HenrikHorpedal <hhorpedal@gmail.com>
2024-08-15 13:47:45 +0200

    feat: try integration tests

--- automated_tests/src/lib.rs
+++ automated_tests/src/lib.rs
@@ -1,6 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub fn add(left: i32, right: i32) -> i32 {
     left + right
 }
+
+pub fn add_two(number: i32) -> i32 {
+    add(number,2)
+}
+
 
 #[cfg(test)]
 mod tests {
---
+++ automated_tests/tests/integration_tests.rs
@@ -0,0 +1,8 @@
+use automated_tests;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4,automated_tests::add_two(2));
+}
+
+
